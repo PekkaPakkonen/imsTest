a40353690c956ed344e15b61dd8f7ef6bbbb9503
Pekka Pakkonen <83037628+PekkaPakkonen@users.noreply.github.com>
2021-07-28 15:00:17 +0000

    Add files via upload

--- src/main/java/catalogPage.java
+++ src/main/java/catalogPage.java
@@ -1,44 +1,63 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class catalogPage {
 
     private WebDriver driver;
+    private JavascriptExecutor exec;
 
-    private By catalogCollapseBtn = By.id("catalogCollapseToggle");
-    private By circuitBreakersBtn= By.cssSelector("#catalogCollapse .l1:first-child > div > button > .px-2:first-child");
-    private By av6Btn = By.cssSelector("#catalogCollapse .l1:first-child .collapse .l2:first-child > div > .btn > .px-2:nth-child(2)");
-    private By av6BCharBtn = By.cssSelector("#catalogCollapse .l1:first-child .collapse .l2:first-child .l3:first-child > .btn");
+    private By l1Buttons = By.cssSelector(".l1 > div > button > .px-2:first-child");
+    private By l2Buttons = By.cssSelector(".l2 div .btn-gray-200 .px-2:nth-child(2)"); //don't change the color of buttons!
+    private By l3Buttons = By.cssSelector(".l3 > div > .btn-gray-100 .px-2:nth-child(2)");
 
     public catalogPage(WebDriver driver) {
         this.driver = driver;
+        exec = (JavascriptExecutor) driver;
     }
 
-    public void clickCatalogCollapseBtn() {
-        driver.findElement(catalogCollapseBtn).click();
+    public void clickAllL1Buttons() throws Exception {
+
+        WebElement[] l1Buttons = getL1Buttons();
+
+        for(WebElement i: l1Buttons) {
+            exec.executeScript("arguments[0].click();", i);
+        }
+
     }
 
-    public void clickCircuitBreakersBtn() {
-        driver.findElement(circuitBreakersBtn).click();
-    }
-    public void clickAv6Btn() {
-        driver.findElement(av6Btn).click();
+    public void clickAllL2Buttons() throws Exception {
+
+        WebElement[] l2Buttons = getL2Buttons();
+
+        for(WebElement i: l2Buttons) {
+            exec.executeScript("arguments[0].click();", i);
+        }
+
     }
 
-    public void clickAv6BBtn() {
-        driver.findElement(av6BCharBtn).click();
+    public void clickAllL3Buttons() throws Exception {
+
+        WebElement[] l3Buttons = getL3Buttons();
+
+        for(WebElement i: l3Buttons) {
+            exec.executeScript("arguments[0].click();", i);
+            Thread.sleep(500);
+        }
+
     }
 
-    public WebElement getWebElementCircuitBreakersBtn() {
-        return driver.findElement(circuitBreakersBtn);
+    public WebElement[] getL1Buttons() {
+        return driver.findElements(l1Buttons).toArray(new WebElement[0]);
     }
 
-    public WebElement getWebAv6Btn() {
-        return driver.findElement(av6Btn);
+    public WebElement[] getL2Buttons() {
+        return driver.findElements(l2Buttons).toArray(new WebElement[0]);
     }
 
-    public WebElement getWebAv6BCharBtn() {
-        return driver.findElement(av6BCharBtn);
+    public WebElement[] getL3Buttons() {
+        return driver.findElements(l3Buttons).toArray(new WebElement[0]);
     }
+
 }
